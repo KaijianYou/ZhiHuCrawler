fd736cb0b72e06f5e5bf97465c5cc9f9050973ad
KaijianYou <kaijianyou@gmail.com>
2017-05-21 22:13:19 +0800

    Add proxies for downloading page

--- html_downloader.py
+++ html_downloader.py
@@ -30,6 +30,7 @@ class HTMLDownloader(object):
             self._login.login(account, password)
 
     def download(self, url, timeout=60):
+        """下载网页"""
         if url is None:
             return None
 
@@ -48,6 +49,40 @@ class HTMLDownloader(object):
 
         page = html.unescape(r.text)
         return page
+
+    def set_proxy(self, proxy):
+        """设置代理"""
+        self._session.proxies.update({'http': proxy})
+
+    def set_proxies(self, proxies, use_https=True):
+        """设置代理池"""
+        from random import choice
+
+        if use_https:
+            self.proxies = [{'http': proxy, 'https': proxy} for proxy in proxies]
+        else:
+            self.proxies = [{'http': proxy} for proxy in proxies]
+
+        def get_with_proxy(url, proxies):
+            proxy = choice(self.proxies)
+            return self._session.orig_get(url, proxies=proxy)
+
+        def post_with_proxy(url, proxies):
+            proxy = choice(self.proxies)
+            return self._session.orig_post(url, proxies=proxy)
+
+        self._session.orig_get = self._session.get
+        self._session.get = get_with_proxy
+        self._session.orig_post = self._session.post
+        self._session.post = post_with_proxy
+
+    def remove_proxy(self):
+        """移除代理池"""
+        self.proxies = None
+        self._session.get = self._session.orig_get
+        self._session.post = self._session.orig_post
+        del self._session.orig_get
+        del self._session.orig_post
 
 
 if __name__ == '__main__':
